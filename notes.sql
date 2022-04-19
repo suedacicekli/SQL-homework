4fd06d09e81e26a8954fa95495d500dca7395de6
suedacicekli <88403704+suedacicekli@users.noreply.github.com>
2022-04-19 05:26:42 +0300

    Add files via upload
    new file

---
+++ notes.sql
@@ -0,0 +1,15 @@
+-- SELECT * FROM film ORDER BY film_id ASC -- Filmleri ID'lerine göre artan şekilde getir.
+
+-- SELECT * FROM film ORDER BY film_id DESC -- Filmleri ID'lerine göre azalan şekilde getir.
+
+-- select * from actor limit 5 -- Aktör tablosundan 5 kayıt getir.
+
+-- select * from actor where first_name = 'Nick' limit 5 -- Aktör tablosundan ismi Nick olan 5 kayıt getir.
+
+-- insert into kullanicilar (name, surname, age) values ('Said Ziya', 'Çağlayan', 6); -- Veri ekleme
+
+-- delete from kullanicilar where id = 5; -- kullanıclar tablosundan id'si 5 olanı sil
+
+-- delete from kullanicilar where id in (6,7); -- kullanıclar tablosundan id'si 6 ve 7 olanı sil
+
+select * from kullanicilar
