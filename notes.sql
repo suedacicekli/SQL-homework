be8a21025965a2913536f8d7dd1ff840a7862c2c
suedacicekli <suedacicekli@gmail.com>
2022-04-23 02:16:15 +0300

    last

--- notes.sql
+++ notes.sql
@@ -4,3 +4,5 @@
 -- SELECT * FROM film WHERE rental_rate = 0.99 AND (replacement_cost =12.99 OR replacement_cost =28.99);
 -- SELECT last_name  FROM customer WHERE first_name = 'Mary';
 -- SELECT * FROM film WHERE NOT length > 50 AND NOT ( rental_rate =2.99 OR rental_rate = 4.99);
+
+--
