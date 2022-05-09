e2987f8043aeb57ad8ad854db9872400f827ecf0
suedacicekli <88403704+suedacicekli@users.noreply.github.com>
2022-05-09 13:48:51 +0300

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -42,3 +42,22 @@ SELECT AVG(rental_rate) FROM film;
 SELECT COUNT(title) FROM film WHERE title ILIKE 'c%';
 SELECT rental_rate,length FROM film WHERE rental_rate = 0.99 ORDER BY length DESC LIMIT 1;
 SELECT COUNT(DISTINCT replacement_cost)  FROM film WHERE length > 150;
+
+-- ÖDEV 7
+-- 1. Film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+SELECT rating, COUNT(*)FROM film
+GROUP BY rating;
+-- 2. film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı
+--50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING  COUNT(*) > 50 ;
+-- 3.customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+-- 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla
+-- şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
